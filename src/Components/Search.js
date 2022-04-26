c2c4ac964e71c4dc2027651dcea0f1951b12d737
deroyyoung <83247276+deroyyoung@users.noreply.github.com>
2022-04-26 18:30:51 -0400

    removed: link from search page

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export default function Search() {
   return (
     <div>
-      <Link to="search">Search</Link>
+      <h1>Search</h1>
     </div>
   );
 }
