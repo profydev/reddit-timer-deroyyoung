900584b36705a2659e059710ab4d0c8ad5258141
deroyyoung <83247276+deroyyoung@users.noreply.github.com>
2022-04-26 18:09:35 -0400

    updated: Header placeholder

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Route, Routes } from 'react-router-dom';
 import GlobalStyles from './Components/styles/globalStyles';
 import Header from './Components/Header';
 import Search from './Components/Search';
+import Main from './Components/Main';
 
 function App() {
   return (
     <>
       <GlobalStyles />
+      <Header />
       <Routes>
-        <Route path="/" element={<Header />} />
-        <Route path="search" element={<Search />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
     </>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <div>
-      <Link to="/">Header</Link>
+      <h1>Header</h1>
     </div>
   );
 }
