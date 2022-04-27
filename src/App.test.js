c53c8306b2f4e6e1fe38ae4f36af101e67da62e8
deroyyoung <83247276+deroyyoung@users.noreply.github.com>
2022-04-27 18:51:28 -0400

    UPDATED: app render test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders App', () => {
-  render(<App />);
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
 });
