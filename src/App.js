0613a6f293b6108a434163abb467b850128f83cb
deroyyoung <83247276+deroyyoung@users.noreply.github.com>
2022-05-04 23:20:57 -0400

    Create app skeleton (#2)
    * added: React Router, configured routes
    
    * added: global styles
    
    * added: react router
    
    * added dependencies
    
    
    * added:  page components components
    
    * fixed: component routing
    
    * removed: unused packages, reinstalled react router
    
    * UPDATED: app render test
    
    * CHANGED: app title and meta description
    
    * CHANGED: FOLDER STRUCTURE

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import GlobalStyles from './globalStyles';
+import Header from './Components/Header/Header';
+import Search from './Components/Search/Search';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <>
+      <GlobalStyles />
+      <Header />
+      <Routes>
+        <Route path="/search" element={<Search />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
