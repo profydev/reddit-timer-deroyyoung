815c9a1561b462366492e409c128f07bbeca1400
deroyyoung <83247276+deroyyoung@users.noreply.github.com>
2022-05-11 20:18:14 -0400

    added theme

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './globalStyles';
 import Header from './Components/Header/Header';
 import Search from './pages/SearchPage.js/Search';
 import Home from './Components/Home/Home';
 
+const theme = {
+  primaryFont: 'Montserrat',
+  secondaryFont: 'Bitter',
+  lightBlack: '#93918F',
+  medBlack: '#787878',
+  black: '#000000',
+  orange: '#FDB755',
+};
+
 function App() {
   return (
-    <>
-      <GlobalStyles />
-      <Header />
-      <Routes>
-        <Route path="/search" element={<Search />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <Routes>
+          <Route path="/search" element={<Search />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </>
+    </ThemeProvider>
   );
 }
 
