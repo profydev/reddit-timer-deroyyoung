69331687d1036f8737854b6028c43e7183375951
deroyyoung <83247276+deroyyoung@users.noreply.github.com>
2022-05-13 17:57:28 -0400

    CHANGED: RENDER BS NAV LINKS AS REACT ROUTER LINK

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -3,31 +3,22 @@ import { Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const StyledLink = styled(Link)`
-  text-decoration: none;
-`;
-
 const StyledNavLink = styled(Nav.Link)`
   &&& {
     color: ${({ theme }) => theme.lightBlack};
     font: 1rem ${({ theme }) => theme.primaryFont};
     font-weight: 400;
     padding-left: 18px;
+    text-decoration: none;
   }
 `;
 
 export default function Navigation() {
   return (
     <Nav style={{ marginLeft: '5px' }}>
-      <StyledLink to="/search/javascript">
-        <StyledNavLink>Search</StyledNavLink>
-      </StyledLink>
-      <StyledLink to="#how-it-works">
-        <StyledNavLink>How it works</StyledNavLink>
-      </StyledLink>
-      <StyledLink to="#about">
-        <StyledNavLink>About</StyledNavLink>
-      </StyledLink>
+      <StyledNavLink as={Link} to="/search/javascript">Search</StyledNavLink>
+      <StyledNavLink as={Link} to="#how-it-works">How it works</StyledNavLink>
+      <StyledNavLink as={Link} to="#about">About</StyledNavLink>
     </Nav>
   );
 }
