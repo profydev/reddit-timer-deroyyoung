a8d09bfdbecf91dbca8c9ea8852322e3c93e5418
deroyyoung <83247276+deroyyoung@users.noreply.github.com>
2022-06-01 10:50:50 -0400

    Fixed Styling

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,40 +3,46 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import footerLogo from '../../Assets/images/sign.svg';
 
+const Container = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  list-style-type: none;
+  padding: 140px 250px;
+`;
+
+const StyledLink = styled.a`
+  color: #636363;
+  font: 0.875rem ${({ theme }) => theme.primaryFont};
+  text-decoration: none;
+`;
+
+const StyledImage = styled.img`
+  margin-left: 46px;
+  margin-top: -8px;
+`;
+
 export default function Footer() {
-  const Container = styled.ul`
-    display: flex;
-    justify-content: space-between;
-    list-style-type: none;
-    padding: 140px 250px;
-  `;
-
-  const StyledLink = styled.a`
-    color: #636363;
-    font: 0.875rem ${({ theme }) => theme.primaryFont};
-    text-decoration: none;
-  `;
-
-  const StyledImage = styled.img`
-      margin-left: 46px;
-      margin-Top: -8px;
-  `;
-
   return (
     <footer>
       <Container>
         <li>
-          <StyledLink href="https://ooloo.io/employers" target="_blank" rel="noreferrer">
+          <StyledLink
+            href="https://ooloo.io/employers"
+            target="_blank"
+            rel="noreferrer"
+          >
             profy.dev
           </StyledLink>
         </li>
         <li>
           <Link to="/">
-            <StyledImage src={footerLogo} alt="logo" />
+            <StyledImage src={footerLogo} alt="magnifying glass " />
           </Link>
         </li>
         <li>
-          <StyledLink as={Link} to="/terms">Terms & Privacy</StyledLink>
+          <StyledLink as={Link} to="/terms">
+            Terms & Privacy
+          </StyledLink>
         </li>
       </Container>
     </footer>
