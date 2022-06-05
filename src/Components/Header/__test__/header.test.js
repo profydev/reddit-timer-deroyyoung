904ffae3dbc19ee43af8da9781d8ca04f986f9c2
deroyyoung <83247276+deroyyoung@users.noreply.github.com>
2022-06-04 21:20:23 -0400

    Changed Header testing

--- src/Components/Header/__test__/header.test.js
+++ src/Components/Header/__test__/header.test.js
@@ -1,47 +1,71 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter, Routes, Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import App from '../../../App';
+import '@testing-library/jest-dom';
 
-const MockHeader = () => (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+const setup = (initialPath = '/') => {
+  let history;
+  let location;
+  render(
+    <MemoryRouter initialEntries={[initialPath]}>
+      <App />
+      <Routes
+        path="*"
+        render={(props) => {
+          history = props.history;
+          location = props.location;
+          return null;
+        }}
+      />
+    </MemoryRouter>,
+  );
+  return { history, location };
+};
 
 describe('Header', () => {
-  it('Logo directs to homepage', () => {
-    render(<MockHeader />);
-
-    const homeLink = screen.getByAltText('logo');
-    userEvent.click(homeLink);
-    expect(screen.getByRole('heading', { name: /home/i })).toBeInTheDocument();
+  it('Clicking logo redirects to home page', () => {
+    setup('/search/javascript');
+    expect(
+      screen.getByRole('link', { name: /search/i }),
+    ).toBeInTheDocument();
+    const logo = screen.getByRole('link', { name: /logo/i });
+    userEvent.click(logo);
+    const homePage = screen.getByRole('heading', { name: /home/i });
+    expect(homePage).toBeInTheDocument();
   });
 
-  it('Search link directs to search page with javascript as default', () => {
-    render(<MockHeader />);
+  it('Clicking search redirects to search page', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router location={history.location} navigator={history}>
+        <App />
+      </Router>,
+    );
 
     const searchLink = screen.getByRole('link', { name: /search/i });
-
+    expect(searchLink).toBeInTheDocument();
     userEvent.click(searchLink);
+    expect(history.location.pathname).toEqual('/search/javascript');
   });
 
-  it('About link', () => {
-    render(<MockHeader />);
+  test.each`
+    link              | hash
+    ${'About'}        | ${'#about'}
+    ${'How it works'} | ${'#how-it-works'}
+  `('clicking $link directs to $link section on homepage', ({ link, hash }) => {
+    const history = createMemoryHistory();
+    render(
+      <Router location={history.location} navigator={history}>
+        <App />
+      </Router>,
+    );
 
-    const aboutLink = screen.getByRole('link', { name: /about/i });
+    const sectionLink = screen.getByRole('link', { name: link });
+    userEvent.click(sectionLink);
 
-    expect(aboutLink);
-    screen.debug();
-  });
-
-  it('How it works link', () => {
-    render(<MockHeader />);
-
-    const howItWorksLink = screen.getByRole('link', { name: /how it works/i });
-
-    expect(howItWorksLink);
-    screen.debug();
+    expect(history.location.hash).toEqual(hash);
   });
 });
