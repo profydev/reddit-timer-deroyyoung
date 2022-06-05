87fe65277ff900de092849139fc2b17d71c2f755
deroyyoung <83247276+deroyyoung@users.noreply.github.com>
2022-06-05 12:53:33 -0400

    testing for links in the footer

--- src/Components/Footer/__test__/footer.test.js
+++ src/Components/Footer/__test__/footer.test.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import Footer from '../Footer';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter, Routes, Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../../../App';
+import '@testing-library/jest-dom';
 
-const MockFooter = () => (
-  <BrowserRouter>
-    <Footer />
-  </BrowserRouter>
-);
+const setup = (initialPath = '/') => {
+  let history;
+  let location;
+  render(
+    <MemoryRouter initialEntries={[initialPath]}>
+      <App />
+      <Routes
+        path="*"
+        render={(props) => {
+          history = props.history;
+          location = props.location;
+          return null;
+        }}
+      />
+    </MemoryRouter>,
+  );
+  return { history, location };
+};
 
 describe('Footer', () => {
-  it('footer page renders', () => {
-    render(<MockFooter />);
-    screen.debug();
+  it('Clicking logo redirects to homepage', () => {
+    setup('/search/javascript');
+
+    const footerLogo = screen.getByRole('img', { name: /magnifying glass/i });
+    userEvent.click(footerLogo);
+    expect(screen.getByRole('heading', { name: /home/i }));
+  });
+
+  it('Clicking terms and privacy links directs to terms page', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router location={history.location} navigator={history}>
+        <App />
+      </Router>,
+    );
+
+    const termsLinks = screen.getByRole('link', { name: /terms/i });
+    userEvent.click(termsLinks);
+
+    expect(history.location.pathname).toEqual('/terms');
   });
 });
